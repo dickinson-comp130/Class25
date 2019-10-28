fafa26f757b6faa5687962ba27d51bf417d2dff6
braughtstu <braughtg@gmail.com>
2019-10-28 15:34:13 -0400

    draft

---
+++ Pig.py
@@ -0,0 +1,22 @@
+import random
+
+"""
+A program that plays a 2-player version of the dice game Pig.
+"""
+
+print("Let's play Pig!")
+
+player1_score = 0
+player2_score = 0
+
+while (player1_score < 100) and (player2_score < 100):
+    
+    # Run player 1's turn
+    player1_score = player1_score + 10   # replace with actual turn total later.
+    
+    # Run player 2's turn
+    player2_score = player2_score + 12   # replace with actual turn total later.
+    
+print("Game over!")
+
+# Print the winner.
